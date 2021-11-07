e0db36ee8e24c6d7a782946dfc0d31c1bc3c35e8
beyondr <jeopatto@gmail.com>
2021-11-06 23:51:08 -0500

    Add wallet check and connect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 // import { ethers } from 'ethers';
 import './App.css';
 
 export default function App() {
-    const wave = () => {};
+    const addSong = () => {};
+    const [currentAccount, setCurrentAccount] = useState('');
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            // Check if we can access window.ethereum
+            const { ethereum } = window;
+
+            if (!ethereum) {
+                console.log("You'll need a wallet to interact! Try MetaMask!");
+                return;
+            } else {
+                console.log('We have the ethereum object!', ethereum);
+            }
+
+            // Check if we can access the user's wallet
+            const accounts = await ethereum.request({ method: 'eth_accounts' });
+            if (accounts.length !== 0) {
+                const account = accounts[0];
+                console.log('Found an authorized account', account);
+                setCurrentAccount(account);
+            } else {
+                console.error('No authorized account found');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window;
+            if (!ethereum) {
+                alert(
+                    "You'll need an Ethereum wallet like MetaMask to submit your song!"
+                );
+                return;
+            }
+            const accounts = await ethereum.request({
+                method: 'eth_requestAccounts',
+            });
+            console.log('🔌 Connected', accounts[0]);
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
 
     return (
         <div className="mainContainer">
@@ -14,9 +64,14 @@ export default function App() {
                     Join the MoshPit and deliver unto us thine banger as a link.
                 </div>
 
-                <button className="waveButton" onClick={wave}>
-                    BESTOW THINE TRACK
-                </button>
+                {/* <button className="waveButton" onClick={addSong}>
+                    Add your song
+                </button> */}
+                {!currentAccount && (
+                    <button className="waveButton" onClick={connectWallet}>
+                        Connect wallet
+                    </button>
+                )}
             </div>
         </div>
     );
